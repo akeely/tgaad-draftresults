e58f33a71686c5de9e14bb0f8f13910e6359973b
Andrew Keely <andrew.keely@gmail.com>
2018-03-24 22:36:19 -0400

    Update for the latest schema

--- loadResults.py
+++ loadResults.py
@@ -5,30 +5,35 @@ import json
 import os
 import boto3
 from jinja2 import Environment, FileSystemLoader
+import ConfigParser
 
 
-def write_results_file(name, cursor):
+def write_results_file(league, cursor):
     cursor.execute("""
-        SELECT p.name, pw.team, p.position, pw.price, pw.time
+        SELECT p.name, t.name, p.position, pw.price, pw.time
         FROM players_won pw
         JOIN players p
-          ON p.playerid=pw.name
-        WHERE league = '{0}'
-    """.format(name))
+          ON p.playerid=pw.playerid
+        JOIN teams t
+          ON pw.teamid=t.id 
+        WHERE pw.leagueid = '{0}'
+    """.format(league['id']))
 
     players = cursor.fetchall()
 
     data = {'data': players}
 
     data_dir = "data/results"
-    data_file = os.path.join(data_dir, "{0}.json".format(name))
+    data_file = os.path.join(data_dir, "{0}.json".format(league['name']))
 
     with open(data_file, 'w') as outfile:
         json.dump(data, outfile)
 
+
 def write_results_page(name, template):
     with open("html/results/{0}.html".format(name), 'w') as outfile:
         outfile.write(template.render(league_name=name))
+
 
 def write_contracts_file(name, cursor):
     cursor.execute("""
@@ -49,9 +54,11 @@ def write_contracts_file(name, cursor):
     with open(data_file, 'w') as outfile:
         json.dump(data, outfile)
 
+
 def write_contracts_page(name, template):
     with open("html/contracts/{0}.html".format(name), 'w') as outfile:
         outfile.write(template.render(league_name=name))
+
 
 # Write a file to S3
 def write_file(bucket, local_path, remote_path=''):
@@ -62,24 +69,31 @@ def write_file(bucket, local_path, remote_path=''):
     with open(local_path, 'r') as data:
         bucket.put_object(Key=remote_path, Body=data, ContentType='text/html')
 
-config = {
-  'user': 'root',
-  'passwd': '',
-  'host': 'localhost',
-  'db': 'auction',
-  'charset': 'utf8'
+
+config = ConfigParser.ConfigParser()
+config.read('db.ini')
+
+section = 'prod'
+
+dbconfig = {
+  'user': config.get(section, 'user'),
+  'passwd': config.get(section, 'password'),
+  'host': config.get(section, 'hostname'),
+  'port': config.getint(section, 'port'),
+  'db': config.get(section, 'schema'),
+  'charset': config.get(section, 'charset')
 }
 
-db = MySQLdb.connect(**config)
+db = MySQLdb.connect(**dbconfig)
 
 cursor = db.cursor()
 
-cursor.execute("SELECT name, sport FROM leagues")
+cursor.execute("SELECT id, name, sport FROM leagues")
 
 leagues = {'baseball': [], 'football': []}
 
 for row in cursor.fetchall():
-    leagues[row[1]].append(row[0])
+    leagues[row[2]].append({'id': row[0], 'name': row[1]})
 
 env = Environment(loader=FileSystemLoader('templates'))
 results_template = env.get_template('results.html')
@@ -87,16 +101,16 @@ contracts_template = env.get_template('contracts.html')
 
 for league in leagues['baseball']:
     write_results_file(league, cursor)
-    write_results_page(league, results_template)
-    write_contracts_file(league, cursor)
-    write_contracts_page(league, contracts_template)
+    write_results_page(league['name'], results_template)
+    write_contracts_file(league['name'], cursor)
+    write_contracts_page(league['name'], contracts_template)
 
 
 for league in leagues['football']:
     write_results_file(league, cursor)
-    write_results_page(league, results_template)
-    write_contracts_file(league, cursor)
-    write_contracts_page(league, contracts_template)
+    write_results_page(league['name'], results_template)
+    write_contracts_file(league['name'], cursor)
+    write_contracts_page(league['name'], contracts_template)
 
 db.close()
 
